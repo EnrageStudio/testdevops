306902c794ee29a31f03ca8fb8bd81723f8ead29
LichKiller <adonis.villalva@outlook.com>
2021-11-26 11:50:26 -0500

    test api

---
+++ src/Api.jsx
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Api = () => {
+    useEffect(() =>{
+        testRest();
+    },[])
+    const [posts, setPosts] = useState([]);
+    const urlGet = (path) => {
+        return axios.get("https://testlk.azurewebsites.net" + path);
+    }
+    const testRest = () => {
+        urlGet("/weatherforecast").then((res) => {
+            setPosts(res.data);
+            console.log(res.data);
+        }).catch((err) => console.log(err))
+    }
+    return(
+        <div>
+            <h1>Prueba API</h1>
+                    <div>
+                        <table border="1">
+                            <thead> 
+                            <tr>
+                                    <td>Ciudad</td>
+                                    <td>Temperatura C</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    posts.map((item) => ((
+<tr>
+                                    <td>{item.summary}</td>
+                                    <td>{item.temperatureC}</td>
+                                </tr>
+                                    )))
+                                }
+                                
+                                
+                            </tbody>
+                        </table>
+                    </div>
+        </div>
+    )
+}
+export default Api;
